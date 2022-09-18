67366b8d66c69585f1ae1c3fc0d9404424efeb2f
Ayoub👾🦆 <ubmagh@pm.me>
2022-09-18 12:33:29 +0100

    added public-reverseProxy url of the exposed pod

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a03efdae691bc46ff80ff7f49b027a3a-64829934.us-east-1.elb.amazonaws.com/api/v0'
 };
 
 /*
